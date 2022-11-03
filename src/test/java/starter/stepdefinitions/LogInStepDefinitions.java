879510142f97d167b34f52496b48085812a36da7
Vanessa Torres <nvanessatr@gmail.com>
2022-11-03 12:52:58 -0500

    added basic step deefinitions

---
+++ src/test/java/starter/stepdefinitions/LogInStepDefinitions.java
@@ -0,0 +1,29 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+
+public class LogInStepDefinitions {
+
+
+    @Managed(driver = "Appium")
+    public WebDriver hisMobileDevice;
+
+    @Before
+    public void set_the_stage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+
+    @Given("John Wick wants to create a new post")
+    public void john_want_to_create_a_new_post() {
+        theActorCalled("Jhon").can(BrowseTheWeb.with(hisMobileDevice));
+    }
+}
---
+++ src/test/resources/features/login/log_in.feature
@@ -0,0 +1,4 @@
+Feature: Log in
+
+  Scenario: Try to log in with not valid credentials
+    Given John Wick wants to create a new post
